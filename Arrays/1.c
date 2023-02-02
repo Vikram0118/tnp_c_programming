2337c408175a423d7af9cfbd22f146c95c42d4be
Vikram0118 <vikramgit001@gmail.com>
2023-02-02 23:37:59 +0530

    did a few more problems

---
+++ Arrays/1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+    int a[] = {5, 1,44, 51, 6, 4, 99, 66 };
+    int small=9999, large=0, i, j;
+    int length = sizeof(a) / sizeof(a[0]);
+
+    // for(i =0; i<length; i++){
+    //     if(a[i] > large){
+    //         large = a[i];
+    //     }
+    //     if(a[i] < small){
+    //         small = a[i];
+    //     }
+    // }
+
+    while(i<j){
+        
+    }
+
+
+    printf("LARGEST : %d AND SMALLEST : %d", large, small);
+}
